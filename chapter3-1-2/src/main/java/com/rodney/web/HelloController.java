93c12d27ff62721b34709331dcac3cd146af80e6
靳中建 <jinzhongjian0@163.com>
2018-01-24 18:10:41 +0800

    使用Thymeleaf模板引擎渲染web视图

---
+++ chapter3-1-2/src/main/java/com/rodney/web/HelloController.java
@@ -0,0 +1,25 @@
+package com.rodney.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @RequestMapping("/")
+    public String index(ModelMap map){
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://blog.didispace.com 这个显示的是数据");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "index";
+    }
+
+}
